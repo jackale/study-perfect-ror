cad9bbedd31a38afd41f229be38e5123deed43ff
jackale <melon119@gmail.com>
2017-11-23 00:08:22 +0900

    Add one_many

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ActiveRecord::Base
+    scope :costly, -> { where("price > ?", 3000) }
+    scope :written_about, ->(theme) { where("name like ?", "%#{theme}%") }
+
+    belongs_to :publisher
 end

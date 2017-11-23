5478908877b7b7a379fa5765de97d92c9df502cb
jackale <melon119@gmail.com>
2017-11-23 12:54:07 +0900

    Add validates

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -4,5 +4,9 @@ class Book < ActiveRecord::Base
 
     belongs_to :publisher
     has_many :book_authors
-    has_many :authors, through: :book_authors
+	has_many :authors, through: :book_authors
+
+	validates :name, presence: true
+	validates :name, length: { maximum: 15 }
+	validates :price, numericality: { greater_than_or_equal_to: 0 }
 end

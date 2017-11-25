496b01475eb5e5d42f474bd737fd5a67e379f339
jackale <melon119@gmail.com>
2017-11-25 23:14:50 +0900

    P.60

--- book_admin/app/models/book.rb
+++ book_admin/app/models/book.rb
@@ -1,12 +1,22 @@
 class Book < ActiveRecord::Base
-    scope :costly, -> { where("price > ?", 3000) }
-    scope :written_about, ->(theme) { where("name like ?", "%#{theme}%") }
+	scope :costly, -> { where("price > ?", 3000) }
+	scope :written_about, ->(theme) { where("name like ?", "%#{theme}%") }
 
-    belongs_to :publisher
-    has_many :book_authors
+	belongs_to :publisher
+	has_many :book_authors
 	has_many :authors, through: :book_authors
 
 	validates :name, presence: true
 	validates :name, length: { maximum: 15 }
 	validates :price, numericality: { greater_than_or_equal_to: 0 }
+
+	before_validation do |book|
+		book.name = self.name.gsub(/Cat/) do |matched|
+			"lonely #{matched}"
+		end
+	end
+
+	after_destroy do |book|
+		Rails.logger.info "Book is deleted: #{book.attributes.inspect}"
+	end
 end
